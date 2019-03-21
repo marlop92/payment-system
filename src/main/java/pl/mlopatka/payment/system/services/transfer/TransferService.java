fa2b8a05b204da0284fc7f897b3ca9414fd382bb
marlop92 <mlopatka.krk@gmail.com>
2019-03-21 02:43:44 +0100

    Move transaction logic to separate method

--- src/main/java/pl/mlopatka/payment/system/services/transfer/TransferService.java
+++ src/main/java/pl/mlopatka/payment/system/services/transfer/TransferService.java
@@ -1,10 +1,11 @@
 package pl.mlopatka.payment.system.services.transfer;
 
+import org.hibernate.Session;
 import pl.mlopatka.payment.system.model.requests.TransferRequest;
 
 import java.time.LocalDateTime;
 
 public interface TransferService {
 
-    void transferMoney(TransferRequest transferRequest, LocalDateTime transferDate);
+    void transferMoney(TransferRequest transferRequest, LocalDateTime transferDate, Session session);
 }
--- src/main/java/pl/mlopatka/payment/system/services/transfer/TransferServiceImpl.java
+++ src/main/java/pl/mlopatka/payment/system/services/transfer/TransferServiceImpl.java
@@ -1,9 +1,6 @@
 package pl.mlopatka.payment.system.services.transfer;
 
 import org.hibernate.Session;
-import org.hibernate.Transaction;
-import pl.mlopatka.payment.system.exceptions.InvalidRestTransaction;
-import pl.mlopatka.payment.system.exceptions.InvalidResultException;
 import pl.mlopatka.payment.system.exceptions.InvalidTransactionState;
 import pl.mlopatka.payment.system.model.Account;
 import pl.mlopatka.payment.system.model.TransferCandidate;
@@ -13,7 +10,6 @@ import pl.mlopatka.payment.system.repo.transfer.TransferRepositoryImpl;
 import pl.mlopatka.payment.system.services.account.AccountService;
 import pl.mlopatka.payment.system.services.account.AccountServiceImpl;
 import pl.mlopatka.payment.system.util.AccountType;
-import pl.mlopatka.payment.system.util.HibernateLifecycleUtil;
 import pl.mlopatka.payment.system.util.TransferUtil;
 
 import java.math.BigDecimal;
@@ -25,7 +21,6 @@ public class TransferServiceImpl implements TransferService {
     private static final String NOT_ENOUGH_MONEY = "Customer doesn't have enough money to do the transfer";
     private static final int EQUALITY = 0;
     private static final String INVALID_RECEIVER_ACCOUNT = "Receiver of Money Transfer cannot have the same account number as a sender";
-    private static final String INTERRUPTED_TRANSACTION = "Transaction interrupted: ";
 
     private final TransferRepository transferRepository;
     private final AccountService accountService;
@@ -35,50 +30,38 @@ public class TransferServiceImpl implements TransferService {
         this.accountService = new AccountServiceImpl();
     }
 
-    public void transferMoney(final TransferRequest transferRequest, final LocalDateTime transferDate) {
-        Session session = HibernateLifecycleUtil.getSessionFactory().openSession();
-        Transaction tx = session.beginTransaction();
-        try {
-            Account senderAccount = accountService.findInternalAccount(transferRequest.getCid(),
-                    transferRequest.getCurrency(), session)
-                    .orElseThrow(() -> new InvalidTransactionState(ACCOUNT_NOT_FOUND));
-            BigDecimal amount = transferRequest.getAmount();
+    public void transferMoney(final TransferRequest transferRequest, final LocalDateTime transferDate,
+                              final Session session) {
 
-            if(senderAccount.getAccountNumber().equals(transferRequest.getReceiverAccount())) {
-                throw new InvalidTransactionState(INVALID_RECEIVER_ACCOUNT);
-            }
+        Account senderAccount = accountService.findInternalAccount(transferRequest.getCid(),
+                transferRequest.getCurrency(), session)
+                .orElseThrow(() -> new InvalidTransactionState(ACCOUNT_NOT_FOUND));
+        BigDecimal amount = transferRequest.getAmount();
 
-            if (senderAccount.getBalance().compareTo(amount) <= EQUALITY) {
-                throw new InvalidTransactionState(NOT_ENOUGH_MONEY);
-            }
-
-            AccountType receiverAccountType = accountService.checkAccountType(transferRequest.getReceiverAccount(),
-                    transferRequest.getCurrency(), session);
-            if (AccountType.NONE.equals(receiverAccountType)) {
-                throw new InvalidTransactionState(ACCOUNT_NOT_FOUND);
-            }
-
-            TransferCandidate transferCandidate = TransferUtil.createTransferCandidate(senderAccount,
-                    transferRequest.getReceiverAccount(), transferRequest.getTitle(), amount, transferDate);
-
-            transferRepository.saveTransfer(transferCandidate, session);
-            accountService.updateAccount(senderAccount.getAccountNumber(), transferRequest.getAmount().negate(), session);
-            if (AccountType.INTERNAL.equals(receiverAccountType)) {
-                accountService.updateAccount(transferRequest.getReceiverAccount(), transferRequest.getAmount(), session);
-            } else {
-                accountService.externalTransfer(transferRequest.getReceiverAccount(), transferRequest.getAmount());
-            }
-            tx.commit();
-
-        } catch (InvalidResultException | InvalidTransactionState ex) {
-            tx.rollback();
-            throw new InvalidRestTransaction(INTERRUPTED_TRANSACTION + ex.getMessage());
-        } catch (RuntimeException ex) {
-            tx.rollback();
-            throw new RuntimeException(INTERRUPTED_TRANSACTION + ex.getMessage());
+        if (senderAccount.getAccountNumber().equals(transferRequest.getReceiverAccount())) {
+            throw new InvalidTransactionState(INVALID_RECEIVER_ACCOUNT);
         }
-        finally {
-            session.close();
+
+        if (senderAccount.getBalance().compareTo(amount) <= EQUALITY) {
+            throw new InvalidTransactionState(NOT_ENOUGH_MONEY);
         }
+
+        AccountType receiverAccountType = accountService.checkAccountType(transferRequest.getReceiverAccount(),
+                transferRequest.getCurrency(), session);
+        if (AccountType.NONE.equals(receiverAccountType)) {
+            throw new InvalidTransactionState(ACCOUNT_NOT_FOUND);
+        }
+
+        TransferCandidate transferCandidate = TransferUtil.createTransferCandidate(senderAccount,
+                transferRequest.getReceiverAccount(), transferRequest.getTitle(), amount, transferDate);
+
+        transferRepository.saveTransfer(transferCandidate, session);
+        accountService.updateAccount(senderAccount.getAccountNumber(), transferRequest.getAmount().negate(), session);
+        if (AccountType.INTERNAL.equals(receiverAccountType)) {
+            accountService.updateAccount(transferRequest.getReceiverAccount(), transferRequest.getAmount(), session);
+        } else {
+            accountService.externalTransfer(transferRequest.getReceiverAccount(), transferRequest.getAmount());
+        }
+
     }
 }
