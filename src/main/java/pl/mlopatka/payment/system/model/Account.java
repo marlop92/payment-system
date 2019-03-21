150a2ee7d911d05a1795500b07bfa2c6214bccd2
marlop92 <mlopatka.krk@gmail.com>
2019-03-21 05:08:24 +0100

    Add AccountServiceTests

--- src/main/java/pl/mlopatka/payment/system/model/Account.java
+++ src/main/java/pl/mlopatka/payment/system/model/Account.java
@@ -2,12 +2,16 @@ package pl.mlopatka.payment.system.model;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.Objects;
 
 public class Account {
 
     private String accountNumber;
     private BigDecimal balance;
     private Currency currency;
+
+    public Account() {
+    }
 
     public Account(String accountNumber, BigDecimal balance, Currency currency) {
         this.accountNumber = accountNumber;
@@ -38,4 +42,21 @@ public class Account {
     public void setCurrency(Currency currency) {
         this.currency = currency;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(accountNumber, account.accountNumber) &&
+                Objects.equals(balance, account.balance) &&
+                Objects.equals(currency, account.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber, balance, currency);
+    }
+
+
 }
